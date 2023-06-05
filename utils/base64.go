814be7e5b9d9aa8e2babbac68d290eb7b7751483
M1chlCZ <m1chlcz18@gmail.com>
2023-06-05 14:56:11 +0200

    Code cleanup

--- utils/base64.go
+++ utils/base64.go
@@ -5,12 +5,10 @@ import (
 )
 
 func EncodePayload(base64 []byte) string {
-	//Base64 Encode
 	return encoder.StdEncoding.EncodeToString(base64)
 }
 
 func DecodePayload(base64 []byte) ([]byte, error) {
-	//Base64 Decode
 	b64 := make([]byte, encoder.StdEncoding.DecodedLen(len(base64)))
 	n, err := encoder.StdEncoding.Decode(b64, base64)
 	if err != nil {
